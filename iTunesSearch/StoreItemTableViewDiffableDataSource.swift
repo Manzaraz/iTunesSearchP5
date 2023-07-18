93306d53381dd8002c3b6b9961fbff115c293534
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-07-18 11:39:32 -0300

    Ffixing titleForHeaderInSection instead of titleForFooterInSection

--- iTunesSearch/StoreItemTableViewDiffableDataSource.swift
+++ iTunesSearch/StoreItemTableViewDiffableDataSource.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @MainActor
 class StoreItemTableViewDiffableDataSource: UITableViewDiffableDataSource<String, StoreItem> {
-    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return snapshot().sectionIdentifiers[section]
     }
 }
